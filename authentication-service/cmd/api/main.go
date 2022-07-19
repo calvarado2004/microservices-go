a6b239d61aa7fe7c123326161a85b665582f4ad8
Carlos Alvarado <carlos-alvarado@outlook.com>
2022-07-18 21:33:22 -0400

    added db func

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -4,9 +4,19 @@ import (
 	"calvarado2004/microservices-go/authentication/data"
 	"database/sql"
 	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+
 )
 
 const webPort = "80"
+
+var counts int64
 
 type Config struct {
 	DB *sql.DB
@@ -17,8 +27,15 @@ type Config struct {
 func main {
 	log.Printn("Starting authentication service on port", webPort)
 
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Could not connect to database")
+	}
 
-	app := Config{}
+	app := Config{
+		DB: conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 
@@ -30,5 +47,45 @@ func main {
 	if err != nil {
 		log.Panic(err)
 	}
-	
+
+}
+
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Error opening database:", err)
+			counts++
+		} else {
+			log.Println("Connected to database")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Retrying in 5 seconds")
+		time.Sleep(5 * time.Second)
+		continue
+
+	}
 }

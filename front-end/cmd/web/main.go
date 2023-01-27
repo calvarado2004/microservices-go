55e0fdeb35ded398a7bb7a8f81c815409f63ff0e
calvarado2004 <carlos-alvarado@outlook.com>
2023-01-26 23:46:10 -0500

    frontend port 8080

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -18,8 +18,8 @@ func main() {
 		render(w, "test.layout.html")
 	})
 
-	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":80", nil)
+	fmt.Println("Starting front end service on port 8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Panic(err)
 	}
